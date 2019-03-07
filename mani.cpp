5de8e9e3cb42f5d2db1ef7313f71379db507abe4
jeyamani543 <48314788+jeyamani543@users.noreply.github.com>
2019-03-06 21:27:39 -1200

    Create mani.cpp

---
+++ mani.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+if(n>0){
+cout<<"positive";}
+else if(n<0){
+cout<<"negative";}
+else if(n==0){
+cout<<"zero";}
+return 0;
+}
